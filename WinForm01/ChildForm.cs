f5575e6a80ed3641de0d7b2e51d5268eab159651
chenqp <13542427@qq.com>
2017-07-11 16:28:02 +0800

    first commit

---
+++ WinForm01/ChildForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm01
+{
+    public partial class ChildForm : Form
+    {
+        // 事件参数类
+        public class TitleChangedEventArgs : EventArgs 
+        {
+            public string Title { get; set; } = "";
+        }
+        
+        // 声明委托
+        public delegate void TitleChangedEventHandler(object sender, TitleChangedEventArgs e);
+
+        // 定义事件
+        public event TitleChangedEventHandler TitleChanged;
+
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var e1 = new TitleChangedEventArgs {Title = "Login sucessed"};
+
+            // 触发事件
+            OnTitleChanged(e1);
+        }
+
+        // 触发事件的方法
+        protected virtual void OnTitleChanged(TitleChangedEventArgs e)
+        {
+            TitleChanged?.Invoke(this, e);
+        }
+    }
+}
